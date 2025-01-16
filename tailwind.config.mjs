3c22888679e1aa26774764117e334cc20fc28ca8
Alehernandezt <j.alehernandezt@gmail.com>
2025-01-15 23:10:02 -0400

    added infinite scroll

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				"loop-scroll": "loop-scroll 10s linear infinite",
+				"loop-scroll-reverse": "loop-scroll-reverse 10s linear infinite",
+			},
+			keyframes: {
+				"loop-scroll": {
+					"0%": { transform: "translateX(0)" },
+					"100%": { transform: "translateX(-100%)" },
+				},
+				"loop-scroll-reverse": {
+					"0%": { transform: "translateX(-100%)" },
+					"100%": { transform: "translateX(0)" },
+				},
+			},
+		},
 	},
 	plugins: [],
-}
+};
