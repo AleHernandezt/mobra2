aa45c6d58b8bcdb51e869051e34436f0c0ee0a77
Alehernandezt <j.alehernandezt@gmail.com>
2025-02-02 22:38:07 -0400

    added fade in and fade out

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,8 +6,8 @@ export default {
             animation: {
                 "loop-scroll": "loop-scroll 10s linear infinite",
                 "loop-scroll-reverse": "loop-scroll-reverse 10s linear infinite",
-                "slideInRight": "slideInRight 0.5s ease-in-out",
-                "slideOutRight": "slideOutRight 0.5s ease-in-out",
+                "fadeOut": "fadeOut 0.5s ease-in-out",
+                "fadeIn": "fadeIn 0.5s ease-in-out",
             },
             keyframes: {
                 "loop-scroll": {
@@ -18,13 +18,13 @@ export default {
                     "0%": { transform: "translateX(-100%)" },
                     "100%": { transform: "translateX(0)" },
                 },
-                "slideInRight": {
-                    "0%": { transform: "translateX(100%)", opacity: "0" },
-                    "100%": { transform: "translateX(0)", opacity: "1" },
+                "fadeOut": {
+                    "0%": { opacity: "1" },
+                    "100%": { opacity: "0" },
                 },
-                "slideOutRight": {
-                    "0%": { transform: "translateX(0)", opacity: "1" },
-                    "100%": { transform: "translateX(100%)", opacity: "0" },
+                "fadeIn": {
+                    "0%": { opacity: "0" },
+                    "100%": { opacity: "1" },
                 },
             },
         },
