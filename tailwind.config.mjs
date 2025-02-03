6ab08940f5076d379ae02ef358b742bf2e439f70
Alehernandezt <j.alehernandezt@gmail.com>
2025-02-02 22:17:18 -0400

    added animation in nav

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,23 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			animation: {
-				"loop-scroll": "loop-scroll 10s linear infinite",
-				"loop-scroll-reverse": "loop-scroll-reverse 10s linear infinite",
-			},
-			keyframes: {
-				"loop-scroll": {
-					"0%": { transform: "translateX(0)" },
-					"100%": { transform: "translateX(-100%)" },
-				},
-				"loop-scroll-reverse": {
-					"0%": { transform: "translateX(-100%)" },
-					"100%": { transform: "translateX(0)" },
-				},
-			},
-		},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            animation: {
+                "loop-scroll": "loop-scroll 10s linear infinite",
+                "loop-scroll-reverse": "loop-scroll-reverse 10s linear infinite",
+                "slideInRight": "slideInRight 0.5s ease-in-out",
+                "slideOutRight": "slideOutRight 0.5s ease-in-out",
+            },
+            keyframes: {
+                "loop-scroll": {
+                    "0%": { transform: "translateX(0)" },
+                    "100%": { transform: "translateX(-100%)" },
+                },
+                "loop-scroll-reverse": {
+                    "0%": { transform: "translateX(-100%)" },
+                    "100%": { transform: "translateX(0)" },
+                },
+                "slideInRight": {
+                    "0%": { transform: "translateX(100%)", opacity: "0" },
+                    "100%": { transform: "translateX(0)", opacity: "1" },
+                },
+                "slideOutRight": {
+                    "0%": { transform: "translateX(0)", opacity: "1" },
+                    "100%": { transform: "translateX(100%)", opacity: "0" },
+                },
+            },
+        },
+    },
+    plugins: [],
 };
